5e4dee7e52f243a8bdfc5ed30d966eef69c7448c
deepak1067 <deepak@knoldus.com>
2023-12-29 16:25:33 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './services/user.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'http-service-angular';
+  pageTitle:string = 'Angular HTTP Client';
+  users = new Array<any>();
+  constructor(private userService:UserService){}
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe((data)=>{
+      this.users = data;
+    })
+  }
+}
